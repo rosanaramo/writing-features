d39d475ad8b9345eb801c2c5239a498986572e83
Rosana Ramo Santos <rosanaramo4@gmail.com>
2023-10-19 16:25:11 -0300

    adding pages

--- writingfeatures/src/test/java/writingfeatures/LoginPage.java
+++ writingfeatures/src/test/java/writingfeatures/LoginPage.java
@@ -4,22 +4,33 @@ import io.cucumber.java.AfterAll;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class LoginPage {
-    private static WebDriver driver = new ChromeDriver();
-//    WebElement email = driver.findElement(By.id("email"));
+    private static WebDriver driver;
+    private WebDriverWait wait;
+
+    private Actions actions;
+    public  LoginPage(){
+       driver  = new ChromeDriver();
+       wait = new WebDriverWait(driver, Duration.ofSeconds(30));
+       actions = new Actions(driver);
+    }
+    WebElement fieldEmail = driver.findElement(By.id("email"));
 //    WebElement password;
 //    WebElement loginButton;
     public void navigateToLoginPage(){
         driver.get("https://front.serverest.dev/login");
     }
     public void fillFieldPassword(String password){
-
     }
-//    public void fillFieldEmail(String email){
-//        Actions action = new Actions(driver);
-//        action.sendKeys(email);
-//    }
+    public void fillFieldEmail(String email){
+        wait.until(ExpectedConditions.elementToBeClickable(fieldEmail));
+        actions.sendKeys(email);
+    }
 //    public void hitLoginButton(){
 //
 //    }
